502417333f049d50302dc80173c4029c1230acd1
Tarun Kochhar <tarunk999@hotmail.com>
2022-08-09 18:10:09 +0530

    Added HackerRank Sol and Link

--- 2D-List-Comprehension.py
+++ 2D-List-Comprehension.py
@@ -4,3 +4,14 @@ list2=[3,4]
 # possible [1,3][1,4][2,3][2,4]
 ans=[[x,y] for x in list1 for y in list2]
 print(ans)
+
+
+# https://www.hackerrank.com/challenges/list-comprehensions/problem?isFullScreen=true
+
+# if __name__ == '__main__':
+#     x = int(input())
+#     y = int(input())
+#     z = int(input())
+#     n = int(input())
+#     res=[[i,j,k] for i in range(0,x+1) for j in range(0,y+1) for k in range(0,z+1) if(i+j+k !=n)]
+#     print(res)
