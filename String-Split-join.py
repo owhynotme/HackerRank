fa9da7c899775e06a932cc00a1f62b9c90d2d9f4
Tarun Kochhar <tarunk999@hotmail.com>
2022-08-21 12:43:27 +0530

    Basic string manuplation

---
+++ String-Split-join.py
@@ -0,0 +1,11 @@
+def split_and_join(line):
+
+# this is a string   
+# this-is-a-string
+
+    # write your code here
+    st=line.split(" ")
+    a="-".join(st)
+    return a
+
+# if __name__ == '__main__':
