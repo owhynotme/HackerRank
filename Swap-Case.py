380efdc8b1d0b2cecc21bcb7c623902fefd2dbe7
Tarun Kochhar <tarunk999@hotmail.com>
2022-08-24 16:05:42 +0530

    Swap case

---
+++ Swap-Case.py
@@ -0,0 +1,10 @@
+def swap_case(s):
+    st = ""
+    for i in s:
+        if i.isupper():
+            i = i.lower()
+        else:
+            i = i.upper()
+        st += "".join(i)
+    return st
+c
